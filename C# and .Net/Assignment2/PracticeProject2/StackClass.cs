5912f766233853e734c517b3389bb7924f8ff45c
Swati Pandit <Swati.Pandit@emtecinc.com>
2022-11-10 19:31:44 +0530

    C# assignment2 added

---
+++ C# and .Net/Assignment2/PracticeProject2/StackClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject2
+{
+    class StackClass
+    {
+        public void getval(int val)
+        {
+            Console.WriteLine(val);
+            getval(++val);
+        }
+        public void execute()
+        {
+            try
+            {
+                getval(0);
+            }
+            catch(StackOverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
