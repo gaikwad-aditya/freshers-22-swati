bcfd9c70552e578eb06f076a29b321abf7cc0300
Swati Pandit <Swati.Pandit@emtecinc.com>
2022-12-06 18:06:31 +0530

    Entity Framework with Web Api assignment

--- ADONetCurdNew/ADONetCurd/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ADONetCurd.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string City { get; set; }
-    }
-}
---
+++ EntityCRUD/WebApiEntity/Controllers/StudentController.cs
@@ -0,0 +1,57 @@
+﻿using DbProject.Interfaces;
+using DbProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiEntity.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentRepository _StudentRepository; 
+
+        public StudentController(IStudentRepository studentRepository)
+        {
+            _StudentRepository = studentRepository;
+        }   
+        [HttpGet]
+        public ActionResult GetStudents()
+        {
+            var students = _StudentRepository.GetStudents();
+            return Ok(students);
+        }
+
+        [HttpGet]
+        public ActionResult GetStudentById(int id)
+        {
+            try
+            {
+                var student = _StudentRepository.GetStudentById(id);
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult AddStudent(Student student)
+        {
+            try
+            {
+                var addedStudent = _StudentRepository.AddStudent(student);
+                return Ok(addedStudent);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+
+        }
+
+        
+    }
+}
